ad8dfd1b4ab9fb0550726ca21f1e0fe7940fed28
Ana <ana.smith@itglobers.com>
2023-02-13 13:17:20 -0500

    feat: configuted graphql query

--- react/components/DepartmentSearch.tsx
+++ react/components/DepartmentSearch.tsx
@@ -1,13 +1,30 @@
-import React from 'react'
-import { useQuery } from 'react-apollo'
-import QUERY_VALUE from '../graphql/getDepartmentGroup.graphql'
+import React, { useState } from 'react';
+import { useQuery } from 'react-apollo';
+import QUERY_VALUE from '../graphql/getDepartmentGroup.graphql';
 
-// import { SearchBar} from 'vtex.store-components'
+import DepartmentGroup from './DepartmentGroup';
+
+import { SearchBar } from 'vtex.store-components'
 
 const DepartmentSearch = () => {
-  const { data, loading } = useQuery(QUERY_VALUE)
-  console.log("Datos del query", data, loading);
-  return loading ? <div>Loading...</div> : <div>Busqueda de departamento</div>
+  const { data, loading } = useQuery(QUERY_VALUE);
+  const [slug, setSlug] = useState('');
+
+  return loading
+  ?
+    <div>Loading...</div>
+  :
+    <div className='flex'>
+      <DepartmentGroup
+        departments={data?.categories}
+        handleSetSlug={setSlug}
+      />
+      <SearchBar
+        customSearchPageUrl={slug}
+        placeholder='Que Departamento busca?'
+        displayMode='search-and-clear-buttons'
+      />
+    </div>
 }
 
 export default DepartmentSearch

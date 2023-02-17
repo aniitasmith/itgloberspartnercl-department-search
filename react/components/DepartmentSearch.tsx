14cf7a182f00cc92c3bf3b02f6e50bbf5f03ae39
Ana <ana.smith@itglobers.com>
2023-02-16 19:58:59 -0500

    feat: styling departments search bar component

--- react/components/DepartmentSearch.tsx
+++ react/components/DepartmentSearch.tsx
@@ -1,20 +1,26 @@
 import React, { useState } from 'react';
 import { useQuery } from 'react-apollo';
+import { useCssHandles } from 'vtex.css-handles'
+import { SearchBar } from 'vtex.store-components'
 import QUERY_VALUE from '../graphql/getDepartmentGroup.graphql';
-
 import DepartmentGroup from './DepartmentGroup';
 
-import { SearchBar } from 'vtex.store-components'
-
 const DepartmentSearch = () => {
+
+  const CSS_HANDLES = [
+    'container-loading',
+    'container-department-search-custom'
+  ]
+  const handles = useCssHandles(CSS_HANDLES)
+
   const { data, loading } = useQuery(QUERY_VALUE);
   const [slug, setSlug] = useState('');
 
   return loading
-  ?
-    <div>Loading...</div>
-  :
-    <div className='flex'>
+    ?
+    <div className={handles['container-loading']}>Loading...</div>
+    :
+    <div className={handles['container-department-search-custom']}>
       <DepartmentGroup
         departments={data?.categories}
         handleSetSlug={setSlug}
@@ -23,6 +29,7 @@ const DepartmentSearch = () => {
         customSearchPageUrl={slug}
         placeholder='Que Departamento busca?'
         displayMode='search-and-clear-buttons'
+        className={handles['department-search-bar-custom']}
       />
     </div>
 }
